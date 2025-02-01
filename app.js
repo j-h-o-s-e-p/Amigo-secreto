a1199805683fe6d1bad1b4c9a0d357085c315924
Jhosep <jcacsire@unsa.edu.pe>
2025-02-01 10:32:34 -0500

    Array para almacenar nombres

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    let amigo = document.getElementById('amigo').value;
+    if(amigo == ""){
+        alert('Por favor, inserte un nombre');
+    }else{
+        amigos.push(amigo);
+    }
+    console.log(amigos);
+    limpiarCaja();
+}
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value = '';
+}
+
