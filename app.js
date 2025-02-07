f20bc576c0cb477b4d5c0d35718af8fc01eefefa
Jhosep <jcacsire@unsa.edu.pe>
2025-02-07 12:57:17 -0500

    Agregando funcion para mostrar la lista de amigos

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ function agregarAmigo(){
     }
     console.log(amigos);
     limpiarCaja();
+    mostrarLista()
+}
+
+function mostrarLista(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for(let i = 0; i < amigos.length; i++){
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
 }
 
 function limpiarCaja(){
