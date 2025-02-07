309620d114d44d6a45922d7e9e6f70e785389cfb
Jhosep <jcacsire@unsa.edu.pe>
2025-02-07 13:23:54 -0500

    Funcion para limpiar elementos

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 let amigos = [];
 
 function agregarAmigo(){
+    limpiarElemento("resultado");
     let amigo = document.getElementById('amigo').value;
     if(amigo == ""){
         alert('Por favor, inserte un nombre');
@@ -15,16 +16,22 @@ function agregarAmigo(){
 
 function mostrarLista(){
     let lista = document.getElementById("listaAmigos");
-    lista.innerHTML = "";
+    limpiarElemento("listaAmigos");
     for(let i = 0; i < amigos.length; i++){
         lista.innerHTML += `<li>${amigos[i]}</li>`;
     }
 }
 
+function limpiarElemento(elemento){
+    let muestra = document.getElementById(elemento);
+    muestra.innerHTML = "";
+}
+
 function sortearAmigo(){
     let indiceRamdom = Math.floor(Math.random() * amigos.length);
     let nombreAmigo = amigos[indiceRamdom];
-    document.getElementById("resultado").innerHTML = nombreAmigo;
+    document.getElementById("resultado").innerHTML = "El amigo secreto sorteado es : " + nombreAmigo;
+    limpiarElemento("listaAmigos");   
 }
 
 function limpiarCaja(){
